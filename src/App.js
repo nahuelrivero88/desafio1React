276dc8338cf916187c089f23da19d44193495b6b
Nahuel Rivero <nahuelriveroch@gmail.com>
2021-12-02 12:06:15 -0300

    desafio Context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar/NavBar";
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import { CartView } from './components/CartView/CartView';
-
-
+import { CartContext } from './context/CartContext';
 
 
 function App() {
+
+  const [carrito, setCarrito] = useState([]);
+  console.log(carrito)
+
+  const agregarCarrito = (item) => {
+    setCarrito([...carrito, item])
+  }
+
+  const quitarCarrito = (id) => {
+    setCarrito(carrito.filter(prod => prod.id !== id));
+  }
+
+  const vaciarCarrito = () => {
+    setCarrito([]);
+  }
+
+  const productoDuplicado = (id) => {
+    return carrito.some(prod => prod.id === id)
+  }
+  
   return (
+    <CartContext.Provider value={{
+      carrito,
+      agregarCarrito,
+      quitarCarrito,
+      vaciarCarrito,
+      productoDuplicado
+    }}>
     <BrowserRouter>
 
       <NavBar />
@@ -23,6 +50,7 @@ function App() {
 
 
     </BrowserRouter>
+    </CartContext.Provider>
   );
 }
 
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { BsCart4 } from "react-icons/bs";
 import './CartWidget.css'
 
+
 export const CartWidget = () => {
+
     return (
         <div>
            <BsCart4 className="carritoIcono"/> 
